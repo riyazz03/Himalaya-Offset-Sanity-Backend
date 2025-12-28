13998559ae68cebf9c474adc58264443779c3430
Riyaz1203 <mohammedriyaz12032003@gmail.com>
2025-12-29 03:05:26 +0530

    order category update

--- schemaTypes/index.ts
+++ schemaTypes/index.ts
@@ -3,11 +3,13 @@ import subcategory from './subcategory'
 import user from './user'
 import otp from './otp'
 import passwordReset from './passwordReset'
+import order from './order'
 
 export const schemaTypes = [
   category, 
   subcategory, 
   user, 
   otp, 
-  passwordReset
+  passwordReset,
+  order
 ]
